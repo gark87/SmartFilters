5e5e74b4f33cb1ec4942713e81d44416d9c86cf8
gark87 <my_another@mail.ru>
2012-02-02 21:54:20 +0300

    robot preferences tab was created

--- src/defaults/preferences/defaults.js
+++ src/defaults/preferences/defaults.js
@@ -1,3 +1,4 @@
-pref("smartfilters.mailing.list.pattern", "%f.lists.%u");
+pref("smartfilters.mailing.list.pattern", "lists.%u");
+pref("smartfilters.robot.pattern", "robots.%u");
 pref("smartfilters.filter.1", "mailing list");
 pref("smartfilters.filter.2", "nothing");
