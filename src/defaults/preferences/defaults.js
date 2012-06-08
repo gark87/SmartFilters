74f06d33cb925f5a50099e4543b1669f9500c844
gark87 <my_another@mail.ru>
2012-06-08 09:22:42 +0400

    correct defaults for filter.1

--- src/defaults/preferences/defaults.js
+++ src/defaults/preferences/defaults.js
@@ -1,6 +1,6 @@
 pref("smartfilters.mailing.list.pattern", "lists.%u");
 pref("smartfilters.robot.pattern", "robots.%u");
-pref("smartfilters.filter.1", "mailing.list");
+pref("smartfilters.filter.1", "mailing list");
 pref("smartfilters.filter.2", "robot");
 pref("smartfilters.max.emails.count", 1000);
 pref("smartfilters.threshold", 5);
